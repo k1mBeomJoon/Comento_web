c8fe6fbf8a26902ec7ffefb42a6dd60e081cb92f
k1mbeomjoon <k1mbeomjoon@example.com>
2025-07-17 16:26:53 +0900

    3주차 django 개발 완료

--- my_project/urls.py
+++ my_project/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', include('product.urls')),
     path('', include('pages.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 
